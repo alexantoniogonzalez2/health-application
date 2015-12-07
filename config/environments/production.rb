db61a0eaa8cad9cb3112b4acf6abbcb082eed8ca
alexantoniogonzalez2 <alexantoniogonzalez2@gmail.com>
2015-12-07 05:44:20 -0300

    compress

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,10 +22,9 @@ AplicacionMedica::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_files = false
 
-  # Compress JavaScripts and CSS.  
-  config.assets.compress = true
+  # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
