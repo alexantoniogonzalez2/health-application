2ee903b845b8a31a75d3b59bc9f531ddbdf4fd0c
alexantoniogonzalez2 <alexantoniogonzalez2@gmail.com>
2016-04-10 21:47:23 -0300

    avance dental

--- app/assets/javascripts/atencion_salud/dental.js
+++ app/assets/javascripts/atencion_salud/dental.js
@@ -3,14 +3,13 @@ $('.img_dental').each(function() { // Notice the .each() loop, discussed below
     $(this).qtip({
     		show: 'click',
     		hide: {
-                fixed: true,
-                delay: 300
+          fixed: true,
+          delay: 300
         },
         content: {
           text: $( "#tooltip_dental" ).html(),
           button: true
-        }
-        ,
+        },
 	   		style: {
 	        classes: 'qtip-rounded qtip-bootstrap'
 	    	},
