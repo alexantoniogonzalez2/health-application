fadc6817bc13c5e2c4f50c6c6c36ac0976d20cf2
alexantoniogonzalez2 <alexantoniogonzalez2@gmail.com>
2016-04-15 23:33:02 -0300

    avance dental 14-04-16

--- app/assets/javascripts/atencion_salud/dental.js
+++ app/assets/javascripts/atencion_salud/dental.js
@@ -1,23 +1,3 @@
-// Grab all elements with the class "hasTooltip"
-$('.img_dental').each(function() { // Notice the .each() loop, discussed below
-    $(this).qtip({
-    		show: 'click',
-    		hide: {
-          fixed: true,
-          delay: 300
-        },
-        content: {
-          text: $( "#tooltip_dental" ).html(),
-          button: true
-        },
-	   		style: {
-	        classes: 'qtip-rounded qtip-bootstrap'
-	    	},
-	    	position: {
-	        target: 'mouse', // Use the mouse position as the position origin
-	        adjust: {
-	          mouse: false
-	        } 
-	      },
-    });
+$("#tooltip_dental").mouseleave(function() {
+  $( "#tooltip_dental" ).hide(); 
 });
